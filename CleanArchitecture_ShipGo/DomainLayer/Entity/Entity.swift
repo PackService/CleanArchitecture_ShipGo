a7d244b6a436007ecc8ad6c9e8f8510738be8ca0
jun12 <jun12@utransfer.com>
2023-07-12 09:45:57 +0900

    chore: 수정 파일 구조

---
+++ CleanArchitecture_ShipGo/DomainLayer/Entity/Entity.swift
@@ -0,0 +1,8 @@
+//
+//  Entity.swift
+//  CleanArchitecture_ShipGo
+//
+//  Created by 이범준 on 2023/07/12.
+//
+
+import Foundation
---
+++ CleanArchitecture_ShipGo/DomainLayer/UseCase/UseCaseImpl/LoginUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  LoginUseCaseImpl.swift
+//  CleanArchitecture_ShipGo
+//
+//  Created by 이범준 on 2023/07/12.
+//
+
+import Foundation
