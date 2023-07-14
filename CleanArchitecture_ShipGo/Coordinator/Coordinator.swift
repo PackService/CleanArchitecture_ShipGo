b4d1fdc475f18771b2121241d8a658d2c25cefb3
jun12 <jun12@utransfer.com>
2023-07-14 10:18:27 +0900

    chore: 정리 파일

---
+++ CleanArchitecture_ShipGo/Coordinator/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  CleanArchitecture_ShipGo
+//
+//  Created by 이범준 on 2023/07/14.
+//
+
+import Foundation
+
+protocol Coordinator: AnyObject { //bumjun
+
+}
--- CleanArchitecture_ShipGo/Resource/TempResource.swift
+++ CleanArchitecture_ShipGo/Resource/TempResource.swift
@@ -6,3 +6,5 @@
 //
 
 import Foundation
+
+// 기존 코나페이 클린 아키텍처 프로젝트 참고해서 base.lproj 폴더 만들어서 LauncyScreen 파일 넣기
