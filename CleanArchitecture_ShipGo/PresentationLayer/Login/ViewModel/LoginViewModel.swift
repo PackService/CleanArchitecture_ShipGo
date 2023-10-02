872bab3c8c4182a57c81dc6d7125f7d8228f2191
Lee Bumjun <69973158+Leebumju@users.noreply.github.com>
2023-10-02 18:38:25 +0900

    feat: 추가 LoginViewModel 로그인 기능

--- CleanArchitecture_ShipGo/PresentationLayer/Login/ViewModel/LoginViewModel.swift
+++ CleanArchitecture_ShipGo/PresentationLayer/Login/ViewModel/LoginViewModel.swift
@@ -12,6 +12,42 @@ import Combine
 class LoginViewModel: BaseViewModel {
     
     private var cancelBag = Set<AnyCancellable>()
-    private let useCase: SignUpUseCaseable = SignUpUseCaseImpl() // 나중에 의존성 주입으로 변경 필요
+    private let useCase: LoginUseCaseable = LoginUseCaseImpl() // 나중에 의존성 주입으로 변경 필요
+    private let shouldLogin = PassthroughSubject<(String, String), Never>()
+    private var loginValidation = PassthroughSubject<UserError, Never>()
     
+    override init() {
+        super.init()
+        bind()
+    }
+    
+    private func bind() {
+        useCase.getErrorSubject()
+            .sink(receiveValue: { [weak self] error in
+                print("loginviewmodel error: \(error)")
+                self?.loginValidation.send(.wrongEmailAndPassword)
+            })
+            .store(in: &cancelBag)
+        
+        shouldLogin
+            .compactMap { [weak self] email, password -> LoginRequestModel? in
+                return .init(email: email,
+                             password: password)
+            }
+            .flatMap(useCase.login(requestModel:))
+            .sink { [weak self] _ in
+                self?.loginValidation.send(.success)
+            }
+            .store(in: &cancelBag)
+    }
+}
+
+extension LoginViewModel {
+    func sendShouldLogin(email: String, password: String) {
+        shouldLogin.send((email, password))
+    }
+    
+    func loginPublisher() -> AnyPublisher<UserError, Never> {
+        return loginValidation.eraseToAnyPublisher()
+    }
 }
